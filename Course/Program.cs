b5a25b9c5d28a9f540beb149458050fe259b874c
Osny <osnymatheusblins@gmail.com>
2021-12-09 10:30:27 -0300

    First commit

---
+++ Course/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.00, 500.00);
+
+            Console.WriteLine(account.Balance);
+
+            // account.Balance = 200; // Não está disponível para classes fora do assembly.
+        }
+    }
+}
